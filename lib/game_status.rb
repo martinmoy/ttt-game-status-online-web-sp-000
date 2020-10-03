7ca11c6ec7dc78d57a2dccb26def2ff8ff72fa16
Martin Moy <martinmoy1991@gmail.com>
2020-10-03 08:00:30 +0000

    Done.

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -1,6 +1,7 @@
 # Helper Method
 def position_taken?(board, index)
-  !(board[index].nil? || board[index] == " " || board[index] == "")
+  #!(board[index].nil? || board[index] == " " || board[index] == "")
+  (board[index] == "X" || board[index]== "O")
 end
 
 WIN_COMBINATIONS = [
@@ -35,11 +36,11 @@ def full?(board)
 end
 
 def draw?(board)
- !won?(board) && full?(board) ? true:false
+ !won?(board) && full?(board) 
 end
 
 def over?(board)
-  won?(board) || draw?(board) ? true:false
+  won?(board) || draw?(board)
 end
 
 def winner(board)
